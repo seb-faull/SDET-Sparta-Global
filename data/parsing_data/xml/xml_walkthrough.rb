29f96256040ebf89696626ec4d3f9a06f031ace8
seb-faull <seb_faull@hotmail.co.uk>
2017-10-19 14:42:04 +0100

    Intro to XML

---
+++ data/parsing_data/xml/xml_walkthrough.rb
@@ -0,0 +1,11 @@
+require 'nokogiri'
+
+xml = Nokogiri::XML(open('xml_menu.xml'))
+
+# xml.search('price').each do |p|
+#   puts p.text
+# end
+# p xml.xpath('//price').text
+xml.xpath('//price').each do |p|
+  puts p.text.gsub('£', '').to_f.class
+end
