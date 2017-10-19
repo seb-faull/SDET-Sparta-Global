3cb444e5e051f6c270082471bd44bd7e40646271
seb-faull <seb_faull@hotmail.co.uk>
2017-10-19 10:03:09 +0100

    Dried u JSON tests and made it reusable

--- data/parsing_data/json/spec/json_test_spec.rb
+++ data/parsing_data/json/spec/json_test_spec.rb
@@ -7,20 +7,35 @@ describe 'JSON example tests' do
   end
 
   it 'should have the key colour for each data object' do
-    expect(@json['colors'][0]).to have_key('color')
-    expect(@json['colors'][1]).to have_key('color')
-    expect(@json['colors'][2]).to have_key('color')
-    expect(@json['colors'][3]).to have_key('color')
-    expect(@json['colors'][4]).to have_key('color')
-    expect(@json['colors'][5]).to have_key('color')
+    @json['colors'].each do |i|
+      expect(i).to have_key('color')
+    end
+    # expect(@json['colors'][0]).to have_key('color')
+    # expect(@json['colors'][1]).to have_key('color')
+    # expect(@json['colors'][2]).to have_key('color')
+    # expect(@json['colors'][3]).to have_key('color')
+    # expect(@json['colors'][4]).to have_key('color')
+    # expect(@json['colors'][5]).to have_key('color')
   end
 
   it "the colour red should have the hex value of '#FF0' " do
-    expect(@json['colors'][2]['code']['hex']).to eq('#FF0')
+    @json['colors'].each do |i|
+      if i["color"] == "red"
+        expect(i["code"]["hex"]).to eq("#FF0")
+      end
+
+    end
+    # expect(@json['colors'][2]['code']['hex']).to eq('#FF0')
   end
 
   it "the colour green should have the RGBA value of '0,255,0,1' " do
-    expect(@json['colors'][5]['code']['rgba']).to eq([0,255,0,1])
+    @json['colors'].each do |i|
+      if i["color"] == "green"
+        expect(i["code"]["rgba"]).to eq([0,255,0,1])
+      end
+
+    end
+    # expect(@json['colors'][5]['code']['rgba']).to eq([0,255,0,1])
   end
 
 end
